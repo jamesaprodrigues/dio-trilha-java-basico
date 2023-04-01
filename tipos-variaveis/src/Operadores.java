4408e9089b6b548d9531b04a8541abfac092b5da
jamesaprodrigues <jamesaprodrigues@gmail.com>
2023-04-01 19:06:07 -0300

    Operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,10 +1,54 @@
 public class Operadores {
 
     public static void main (String[] args) {
-        String nomeCompleto = "LINGUAGEM " + "JAVA";
+        int numero = 5;
 
-        System.out.println(nomeCompleto);
+        //x repetição 
+        numero ++;
+        //numero = numero =1;
+        System.out.println(numero);
+        
+        //inversão verificação logica.
+        boolean variavel = false;
+
+        variavel = !variavel;
+
+        System.out.println(! variavel);
+
+        int a,b;
+
+        a = 5;
+        b = 6;
+        // operacdor ternario.
+        String  resultado= a==b ?"verdadeiro" : "falso";
+
+        //if(a==b)
+         //resultado = "verdadeiro";
+        //else 
+         //resultado = "falso";
+         
+        // Operadores logicos.
+        System.out.println(resultado);
+
+        Boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if(condicao1 && ( 7 > 4 )){
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+        }
+
+        System.out.println("fim");
+
+
 
     }
-    
+
 }
+
+    
+
